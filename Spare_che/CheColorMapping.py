0d2d179d8308ee8ed3ac96605110fa841eb747fd
Hajime Kawahara <divrot@gmail.com>
2019-09-17 12:38:01 +0900

    added cupy version module

--- Spare_che/CheColorMapping.py
+++ Spare_che/CheColorMapping.py
@@ -1,27 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import module
-from module import *
+import cumodule
+from cumodule import *
+importlib.reload(cumodule)
 import importlib
-importlib.reload(module)
 import readpng as rpng
+import colour 
+from colour.plotting import *
+import itertools
 
 img=rpng.get_img("./car.png")
 
-np.shape(img)
-
 fig=plt.figure()
-timg=img[::8,::8,:]
+timg=img[::2,::2,:]
 plt.imshow(timg)
 plt.savefig("carcar.png")
-plt.clear()
 
 Nx, Ny, Nc = np.shape(timg)
-N_data = 100
-rand_now = module.random_generator(N_data, Nx, Ny)
-dRGB, g=rand_now.make_colordata(timg,20) 
+N_data = 300
+rand_now = cumodule.random_generator(N_data, Nx, Ny)
+dRGB, g=rand_now.make_colordata(timg,80) 
 g=np.array(g)
-
 
 I_init=np.ones((Nx,Ny))
 yR = mfista_func(I_init, dRGB[:,0], g, lambda_l1= 1e-6, lambda_tsv= 1e-3)
@@ -29,26 +28,12 @@ I_init=np.ones((Nx,Ny))
 yG = mfista_func(I_init, dRGB[:,1], g, lambda_l1= 1e-6, lambda_tsv= 1e-3)
 I_init=np.ones((Nx,Ny))
 yB = mfista_func(I_init, dRGB[:,2], g, lambda_l1= 1e-6, lambda_tsv= 1e-3)
-
 y=np.array([yR.T,yG.T,yB.T]).T
-np.shape(y)
 
 plt.imshow(y)
 plt.savefig("carL1tsv.png")
 
-
-ti=timg.reshape(46*51,3)
-
-
-# install colour package
-# pip install colour-science
-
-# In[12]:
-
-
-import colour 
-from colour.plotting import *
-import itertools
+ti=timg.reshape(np.shape(timg)[0]*np.shape(timg)[1],3)
 #XYZからxyへ変換
 ti=timg.reshape(np.shape(timg)[0]*np.shape(timg)[1],3)
 XYZ = colour.sRGB_to_XYZ(ti)
@@ -70,8 +55,14 @@ yi=y.reshape(np.shape(y)[0]*np.shape(y)[1],3)
 XYZ = colour.sRGB_to_XYZ(yi)
 xy = colour.XYZ_to_xy(XYZ)
 
+print(np.shape(yi),np.shape(xy[:,0]),np.shape(xy[:,1]))
 ax=fig.add_subplot(122,aspect=1.0)
-ax.scatter(xy[:,0], xy[:,1],facecolors=yi,alpha=1,s=2)
+
+yim=np.copy(yi)
+yim[yim>1.0]=1.0
+yim[yim<0.0]=0.0
+
+ax.scatter(xy[:,0], xy[:,1],facecolor=yim,alpha=1,s=2)
 ax.set_xlim(0.15,0.62)
 ax.set_ylim(0.2,0.62)
 ax.set_title("reconstruct")
